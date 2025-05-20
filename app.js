6e8f5814b97831930c16dc08d7a8e570e324ec62
Jacob_Cozens <jacobcozens@rocketmail.com>
2025-05-20 15:31:58 -0400

    Add secret key

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 const { PORT = 3001 } = process.env;
