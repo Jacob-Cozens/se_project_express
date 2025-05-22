7bdb1b4f0ce2a517dbde8f23008fd391bcfe64ec
Jacob_Cozens <jacobcozens@rocketmail.com>
2025-05-22 11:38:55 -0400

    Set up server crash testing

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,12 @@ const auth = require("../middlewares/auth");
 const { NOT_FOUND } = require("../utils/errors");
 const { loginUser, createUser } = require("../controllers/users");
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 router.post("/signin", loginUser);
 router.post("/signup", createUser);
 
